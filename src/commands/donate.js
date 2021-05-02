9a913d7b34f0602bd67b7efdbe04d82404f7cdd8
Zeko <41507889+ZekoTheFox@users.noreply.github.com>
2021-05-02 13:42:05 -0700

    Fix typo in donation information.

--- src/commands/donate.js
+++ src/commands/donate.js
@@ -18,6 +18,6 @@ module.exports = class DonateCommand extends libCommand.Command {
         message.channel.send(new MessageEmbed()
         .setTitle('This And That | Donating')
         .setColor(0x00ff00)
-        .setDescription('Thank you for taking notice for donating to this bot.\n\nYou can donate via my PayPal, which you can find [here](https://paypal.me/Foxyrina)!\n\nDonating helps me fund service and server costs for running this bot, and helps motivate me to continuiously improve the user experience.'));
+        .setDescription('Thank you for taking notice for donating to this bot.\n\nYou can donate via my PayPal, which you can find [here](https://paypal.me/Foxyrina)!\n\nDonating helps me fund this service and covers server costs and API requests for running this bot, and helps motivate me to continuiously improve the user experience.'));
     }
 }
