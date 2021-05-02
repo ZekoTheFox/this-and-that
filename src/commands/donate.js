cdb6fbcd7eba99aa6d600ee89590185a9865abc6
Zeko <41507889+ZekoTheFox@users.noreply.github.com>
2021-05-02 13:38:01 -0700

    Fix some meta information.

--- src/commands/donate.js
+++ src/commands/donate.js
@@ -3,14 +3,14 @@ const libCommand = require('../command.js');
 // Constants
 const { MessageEmbed } = require('discord.js');
 
-module.exports = class PingCommand extends libCommand.Command {
+module.exports = class DonateCommand extends libCommand.Command {
     constructor() {
         super();
         // Meta Command Information
         this.internalCommandEnabled = true;
         // Help Information
-        this.helpCommandTitle = 'Ping';
-        this.helpCommandDescription = 'Ping from Discord to this bot.';
+        this.helpCommandTitle = 'Donations';
+        this.helpCommandDescription = 'Shows donation info, about how you can donate to this bot\'s author to help fund for service and server costs.';
         this.helpCommandColor = parseInt('0x' + botConfig.botConfig.embedColor);
     }
 
@@ -19,6 +19,6 @@ module.exports = class PingCommand extends libCommand.Command {
         message.channel.send(new MessageEmbed()
         .setTitle('This And That | Ping')
         .setColor(0xffffff)
-        .setDescription(`Ping: ${client.ws.ping}ms`));
+        .setDescription('Thank you for taking notice for donating to this bot.'));
     }
 }
